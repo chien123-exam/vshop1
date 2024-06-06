684da0dfcca4482129e1c66567b763c6346ee587
nguyentientruong0201 <84010674+nguyentientruong0201@users.noreply.github.com>
2024-06-06 10:41:05 +0700

    login and logout

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <Narbar />
+
+    <Sidebar />
+    
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Narbar from './Narbar.vue'
+import Sidebar from './Sidebar.vue'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
